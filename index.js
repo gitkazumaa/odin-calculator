988329b88b3972569435df9b42ccdfac3ab7ab70
gitkazumaa <kazumaalternate@gmail.com>
2023-06-17 11:50:29 -0700

    Add operate function

--- index.js
+++ index.js
@@ -9,3 +9,10 @@ const divide = (num1, num2) => num1 / num2;
 let num1;
 let num2;
 let operator;
+
+const operate = (num1, num2, operator) => {
+    return (operator === "+" ? add(num1, num2)
+        : operator === "-" ? subtract(num1, num2)
+        : operator === "*" ? multiply(num1, num2)
+        : divide(num1, num2));
+}
