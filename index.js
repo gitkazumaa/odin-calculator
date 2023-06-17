d968e9b627d0e6b7f16081acc69478b98045fd55
gitkazumaa <kazumaalternate@gmail.com>
2023-06-17 12:29:27 -0700

    Create populateDisplay

--- index.js
+++ index.js
@@ -11,8 +11,19 @@ let num2;
 let operator;
 
 const operate = (num1, num2, operator) => {
-    return (operator === "+" ? add(num1, num2)
+    return operator === "+" ? add(num1, num2)
         : operator === "-" ? subtract(num1, num2)
         : operator === "*" ? multiply(num1, num2)
-        : divide(num1, num2));
+        : divide(num1, num2);
 }
+
+const displayTextArea = document.querySelector(".display-text-area");
+let displayValue;
+
+const populateDisplay = (num) => {
+    displayValue ? displayValue += num : displayValue = num;
+    displayTextArea.textContent = displayValue;
+}
+
+const numberButtons = document.querySelectorAll("button")
+numberButtons.forEach(button => button.addEventListener("click", () => populateDisplay(button.textContent)))
